b07f14196c07bb9ce1b75ebf31d8d95f930936dd
Beatriz Cangas Perez <38326583+beatrizcangas41@users.noreply.github.com>
2020-03-22 21:39:06 -0400

    Project Structure Modified - v3

---
+++ src/main/java/controllers/ForgotPasswordController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ForgotPasswordController {
+    public Button goBackButton;
+    public Button loginButtonPressed;
+    public PasswordField passwordField;
+    public TextField usernameTextField;
+
+    public void confirmEmail(ActionEvent actionEvent) {
+    }
+
+    public void goBackButton(ActionEvent actionEvent) {
+    }
+
+    public void loginButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void forgotPassword(ActionEvent actionEvent) {
+    }
+
+    public void registerButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/controllers/PasswordResetController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class PasswordResetController {
+    public Button confirmPassword;
+
+    public void confirmPassword(ActionEvent actionEvent) {
+    }
+
+    public void cancelButton(ActionEvent actionEvent) {
+    }
+}
