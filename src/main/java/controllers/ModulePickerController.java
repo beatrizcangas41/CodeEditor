a2fcb15afa0725db3dbe1bceda5310fe319dac28
Beatriz Cangas Perez <38326583+beatrizcangas41@users.noreply.github.com>
2020-04-04 18:02:00 -0400

    UI Update

---
+++ src/main/java/controllers/ModulePickerController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+
+public class ModulePickerController {
+
+    @FXML
+    public TableView tableView;
+    public Button goBackButton;
+    public TabPane tabPane;
+
+    public void goBack(ActionEvent actionEvent) {
+
+    }
+
+}
